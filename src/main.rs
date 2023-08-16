827d9686684d8123d268ad600e427db90f4bada5
AdrisGithub <adrianjohn6543@gmail.com>
2023-08-16 15:03:36 +0200

    add a really simple TCP listener that console logs requests and sends out a 200 OK Response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        let reader = BufReader::new(&stream);
+        let request = reader
+            .lines()
+            .map(|line| line.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect::<Vec<String>>();
+        println!("{:#?}", request);
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
