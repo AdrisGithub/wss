1c7a0bc1c21ccfdc65e97d1b67ae2d8f3b085911
AdrisGithub <adrianjohn6543@gmail.com>
2023-08-16 15:54:00 +0200

    started to reformat the code to make it more readable

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
-use std::io::{BufRead, BufReader, Write};
-use std::net::TcpListener;
+pub mod net;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    net::server::read();
+}
 
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        let reader = BufReader::new(&stream);
-        let request = reader
-            .lines()
-            .map(|line| line.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect::<Vec<String>>();
-        println!("{:#?}", request);
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
-        stream.write_all(response.as_bytes()).unwrap();
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 }
