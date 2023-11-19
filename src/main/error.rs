08c25dbfd64a6efd934a33e99fa596f0d794a3e5
Adrian John <adrianjohn6543@gmail.com>
2023-11-19 11:09:28 +0100

    manually implement Debug for WSSError

--- src/main/error.rs
+++ src/main/error.rs
@@ -1,6 +1,7 @@
+use std::fmt::{Debug, Formatter};
 use std::io::Error;
 
-#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Debug, Hash, Default)]
+#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Hash, Default)]
 pub struct WSSError(String);
 
 impl WSSError {
@@ -12,5 +13,11 @@ impl WSSError {
 impl From<Error> for WSSError {
     fn from(value: Error) -> Self {
         Self::new(value.to_string())
+    }
+}
+
+impl Debug for WSSError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
