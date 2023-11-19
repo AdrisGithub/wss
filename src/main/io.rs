9ea80405f7963e796f035b1467dc617d7069995e
Adrian John <adrianjohn6543@gmail.com>
2023-11-19 19:20:25 +0100

    add functionality to read, create, edit and delete files

--- src/main/io.rs
+++ src/main/io.rs
@@ -1,8 +1,13 @@
-use std::fs::{create_dir, read_dir, read_to_string};
+use std::fs::{create_dir, File, read_dir, read_to_string, remove_file};
+use std::io::Write;
 use std::ops::Add;
 
+use aul::{log, log_info};
+use aul::level::Level;
+use aul::log_error;
 
 const DIRECTORY: &str = "./files";
+
 pub fn init() {
     let dir = read_dir(DIRECTORY);
     if dir.is_err() {
@@ -13,9 +18,36 @@ pub fn init() {
 
 pub fn get_file(path: &str) -> Option<String> {
     read_to_string(get_actual_path(path))
-        .map_err(|err|println!("{}",err)).ok()
+        .map(|str|{log_info!("{}",str); str})
+        .map_err(|err| println!("{}", err)).ok()
 }
 
-pub fn get_actual_path(path: &str) -> String{
+pub fn get_actual_path(path: &str) -> String {
     DIRECTORY.to_string().add(path)
+}
+
+pub fn create_file(path: &str, content: &String) -> bool {
+    let created = File::create(get_actual_path(path))
+        .map_err(|err| log_error!("{}",err))
+        .is_ok();
+    if created {
+        edit_file(path, content)
+    } else {
+        false
+    }
+}
+
+pub fn edit_file(path: &str, content: &String) -> bool {
+    if let Ok(mut file) = File::create(get_actual_path(path)).map_err(|err| log_error!("{}",err)) {
+        log_info!("{}",content);
+        log_error!("{:?}",file.write(content.as_bytes()));
+        log_error!("{:?}",file.flush());
+        true
+    } else {
+        false
+    }
+}
+
+pub fn delete_file(path: &str) {
+    let _ = remove_file(get_actual_path(path));
 }
