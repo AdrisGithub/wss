46eaa00ee64ee107320fb58a192f04d959d8d815
Adrian John <adrianjohn6543@gmail.com>
2023-12-13 20:03:44 +0100

    add a middleware trait

---
+++ src/main/middleware.rs
@@ -0,0 +1,11 @@
+use whdp::{Request, Response};
+/// # Safety
+/// this is really unsafe
+pub unsafe trait Middleware{
+    fn on_request(&self, req: Request) -> Request {
+        req
+    }
+    fn on_response(&self, resp: Response) -> Response {
+        resp
+    }
+}
