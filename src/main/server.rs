0815b791c89fc326d71a2e58badc5a2b514ec033
Adrian John <adrianjohn6543@gmail.com>
2023-12-14 19:42:30 +0100

    add an Iterator trait for Server

--- src/main/server.rs
+++ src/main/server.rs
@@ -1,9 +1,9 @@
 use std::io;
 use std::io::Write;
-use std::net::{SocketAddr, TcpListener};
+use std::net::{Incoming, SocketAddr, TcpListener};
 
-use whdp::resp_presets::ok;
-use whdp::TryRequest;
+use whdp::resp_presets::{internal_server_error, ok};
+use whdp::{Request, TryRequest};
 
 use crate::middleware::Middleware;
 
@@ -14,11 +14,9 @@ pub struct Server {
 
 impl Server {
     pub fn start(&self) {
-        for stream in self.listener.incoming() {
-            let mut stream = stream
-                .unwrap();
-            let mut req = stream.try_to_request()
-                .unwrap();
+        for stream in self.incoming() {
+            let mut stream = stream.unwrap();
+            let mut req = stream.try_to_request().unwrap();
             for middle in self.middlewares.iter() {
                 req = middle.on_request(req);
             }
@@ -33,6 +31,10 @@ impl Server {
             let _ = stream.write_all(resp.to_string().as_bytes());
         }
     }
+
+    pub fn incoming(&self) -> Incoming<'_> {
+        self.listener.incoming()
+    }
 }
 
 impl TryFrom<SocketAddr> for Server {
@@ -43,4 +45,23 @@ impl TryFrom<SocketAddr> for Server {
             middlewares: Vec::new(),
         })
     }
+}
+
+impl Iterator for Server {
+    type Item = Request;
+    fn next(&mut self) -> Option<Self::Item> {
+        let opt = self.incoming().next();
+        if let Some(Ok(mut stream)) = opt {
+            if let Ok(req) = stream.try_to_request() {
+                return Some(req);
+            } else {
+                let _ = stream.write_all(internal_error().as_bytes());
+            }
+        }
+        self.next()
+    }
+}
+
+fn internal_error() -> String {
+    internal_server_error("Internal Server Error".into()).to_string()
 }
