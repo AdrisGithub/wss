635229f57e7b42d4f3410fa3353f5b8233506832
Adrian John <adrianjohn6543@gmail.com>
2023-12-17 20:17:48 +0100

    add a builder for RawServlet

--- src/main/raw_servlet.rs
+++ src/main/raw_servlet.rs
@@ -1,10 +1,11 @@
 use std::io::Write;
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 
 use aul::error;
 use aul::level::Level;
 use aul::log;
 
+use crate::error::WBSLError;
 use crate::helper::{parse_stream, RawFunction};
 
 pub struct RawServlet {
@@ -18,6 +19,51 @@ impl RawServlet {
             let result = (self.func)(string);
             let _ = stream.write_all(result.as_bytes());
         }
+    }
+    pub fn builder() -> RawServletBuilder {
+        RawServletBuilder::default()
+    }
+}
+
+#[derive(Default)]
+pub struct RawServletBuilder {
+    listener: Option<TcpListener>,
+    func: Option<RawFunction>,
+}
+
+impl RawServletBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_func(mut self, func: RawFunction) -> Self {
+        self.func = Some(func);
+        self
+    }
+    pub fn bind<A: ToSocketAddrs>(mut self, addr: A) -> Result<RawServlet, WBSLError> {
+        let binding = TcpListener::bind(addr);
+        match binding {
+            Ok(listener) => self.listener = Some(listener),
+            Err(_error) => return Err(WBSLError),
+        }
+        self.build()
+    }
+    pub fn with(mut self, listener: TcpListener) -> Self {
+        self.listener = Some(listener);
+        self
+    }
+    pub fn build(self) -> Result<RawServlet, WBSLError> {
+        if self.validate() {
+            Ok(RawServlet {
+                func: self.func.unwrap(),
+                listener: self.listener.unwrap(),
+            })
+        } else {
+            Err(WBSLError)
+        }
+    }
+    fn validate(&self) -> bool {
+        self.listener.is_some() && self.func.is_some()
     }
 }
 
