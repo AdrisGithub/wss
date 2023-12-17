31e4b38618eaab88d8a11c115d7682abee1e80ee
Adrian John <adrianjohn6543@gmail.com>
2023-12-17 20:18:02 +0100

    add a builder for ServletBuilder

--- src/main/ser_servlet.rs
+++ src/main/ser_servlet.rs
@@ -1,16 +1,67 @@
 use std::io::Write;
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 
 use aul::error;
 use aul::level::Level;
 use aul::log;
 use wjp::{Deserialize, Serialize};
 
+use crate::error::WBSLError;
 use crate::helper::{parse_stream, SerializableFunction};
 
 pub struct SerializeServlet<I: Deserialize, O: Serialize> {
     listener: TcpListener,
     func: SerializableFunction<I, O>,
+}
+
+pub struct SerializeServletBuilder<I: Deserialize, O: Serialize> {
+    listener: Option<TcpListener>,
+    func: Option<SerializableFunction<I, O>>,
+}
+
+impl<I: Deserialize, O: Serialize> SerializeServletBuilder<I, O> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_func(mut self, func: SerializableFunction<I, O>) -> Self {
+        self.func = Some(func);
+        self
+    }
+    pub fn bind<A: ToSocketAddrs>(mut self, addr: A) -> Result<SerializeServlet<I, O>, WBSLError> {
+        let binding = TcpListener::bind(addr);
+        match binding {
+            Ok(listener) => self.listener = Some(listener),
+            Err(_error) => return Err(WBSLError),
+        }
+        self.build()
+    }
+    pub fn with(mut self, listener: TcpListener) -> Self {
+        self.listener = Some(listener);
+        self
+    }
+    pub fn build(self) -> Result<SerializeServlet<I, O>, WBSLError> {
+        if self.validate() {
+            Ok(SerializeServlet {
+                func: self.func.unwrap(),
+                listener: self.listener.unwrap(),
+            })
+        } else {
+            Err(WBSLError)
+        }
+    }
+    fn validate(&self) -> bool {
+        self.listener.is_some() && self.func.is_some()
+    }
+}
+
+impl<I: Deserialize, O: Serialize> Default for SerializeServletBuilder<I, O> {
+    fn default() -> Self {
+        Self {
+            func: None,
+            listener: None,
+        }
+    }
 }
 
 impl<I: Deserialize, O: Serialize> SerializeServlet<I, O> {
@@ -19,6 +70,9 @@ impl<I: Deserialize, O: Serialize> SerializeServlet<I, O> {
             let output = (self.func)(input);
             let _ = stream.write(output.json().as_bytes());
         }
+    }
+    pub fn builder() -> SerializeServletBuilder<I, O> {
+        SerializeServletBuilder::new()
     }
 }
 
