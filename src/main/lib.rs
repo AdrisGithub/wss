907cb68c668d699d4cdec6154019d4e59c9e7117
Adrian John <adrianjohn6543@gmail.com>
2023-12-17 23:34:05 +0100

    sort lib.rs

--- src/main/lib.rs
+++ src/main/lib.rs
@@ -1,9 +1,9 @@
 pub mod error;
 pub mod helper;
+pub mod methods;
 pub mod middleware;
 pub mod raw_servlet;
 pub mod router;
 pub mod ser_servlet;
 pub mod server;
 pub mod servlet;
-pub mod methods;
