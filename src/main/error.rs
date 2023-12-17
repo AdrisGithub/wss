f340ca536978043d92a14f0c50ac58d1a147d9f4
Adrian John <adrianjohn6543@gmail.com>
2023-12-17 19:39:07 +0100

    add Display and Debug impl for WBSL Error

--- src/main/error.rs
+++ src/main/error.rs
@@ -1,1 +1,15 @@
+use std::fmt::{Debug, Display, Formatter};
+
 pub struct WBSLError;
+
+impl Debug for WBSLError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "WBSL ERROR")
+    }
+}
+
+impl Display for WBSLError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(self,f)
+    }
+}
