b104924ba9f0fc7bdba37585ea975faf55ac2810
Adrian John <adrianjohn6543@gmail.com>
2023-12-17 22:56:45 +0100

    add a serverbuilder

--- src/main/helper.rs
+++ src/main/helper.rs
@@ -1,9 +1,15 @@
+use std::fmt::Write;
 use std::io::{BufRead, BufReader};
 use std::net::TcpStream;
+use std::time::{SystemTime, UNIX_EPOCH};
 
+use aul::level::Level;
+use aul::log;
+use aul::sensitive::Sens;
 use whdp::{Request, Response};
 
 use crate::error::WBSLError;
+use crate::middleware::Middleware;
 
 pub type HTTPFunction = fn(Request) -> Response;
 pub type RawFunction = fn(String) -> String;
@@ -15,3 +21,55 @@ pub(crate) fn parse_stream(stream: &TcpStream) -> Result<String, WBSLError> {
     reader.consume(received.len());
     String::from_utf8(received).map_err(|_e| WBSLError)
 }
+
+pub(crate) struct Logger(Level);
+
+impl From<Level> for Logger {
+    fn from(value: Level) -> Self {
+        Self(value)
+    }
+}
+
+unsafe impl Middleware for Logger {
+    fn on_request(&self, req: Request) -> Request {
+        log!(self.0,"Received Request, {}",Sens(&req));
+        req
+    }
+    fn on_response(&self, resp: Response) -> Response {
+        log!(self.0,"Received Request, {}", Sens(&resp));
+        resp
+    }
+}
+
+
+pub(crate) struct AdditionalHeaders(String, String);
+
+impl From<(String, String)> for AdditionalHeaders {
+    fn from(value: (String, String)) -> Self {
+        Self(value.0, value.1)
+    }
+}
+
+unsafe impl Middleware for AdditionalHeaders {
+    fn on_request(&self, req: Request) -> Request {
+        req
+    }
+    fn on_response(&self, mut resp: Response) -> Response {
+        resp.add_header((String::from("Server"), self.0.clone()));
+        resp.add_header((String::from("Content-Type"), self.1.clone()));
+        resp.add_header((String::from("Date"), get_current_time()));
+
+        resp
+    }
+}
+
+pub(crate) fn get_current_time() -> String {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let mut out = String::new();
+    let _ = write!(out, "{:?}", since_the_epoch);
+    out
+}
+
--- src/main/server.rs
+++ src/main/server.rs
@@ -8,6 +8,7 @@ use aul::log;
 use whdp::{Request, TryRequest};
 use whdp::resp_presets::{internal_server_error, not_found};
 
+use crate::helper::{AdditionalHeaders, Logger};
 use crate::middleware::Middleware;
 use crate::router::Router;
 
@@ -15,6 +16,39 @@ pub struct Server {
     listener: TcpListener,
     middlewares: Vec<Box<dyn Middleware>>,
     router: Router,
+}
+
+pub struct ServerBuilder {
+    socket: Option<SocketAddr>,
+    middlewares: Vec<Box<dyn Middleware>>,
+    router: Router,
+}
+
+impl ServerBuilder {
+    pub fn new() -> Self {
+        ServerBuilder::default()
+    }
+    pub fn add_middle(&mut self, middle: Box<dyn Middleware>) -> &mut Self {
+        self.middlewares.push(middle);
+        self
+    }
+
+    pub fn with_logging(&mut self, level: Level) -> &mut Self {
+        self.add_middle(Box::new(Logger::from(level)))
+    }
+    pub fn with_auto_headers(&mut self, app_name: String, content_type: String) -> &mut Self {
+        self.add_middle(Box::new(AdditionalHeaders::from((app_name, content_type))))
+    }
+}
+
+impl Default for ServerBuilder {
+    fn default() -> Self {
+        Self {
+            socket: None,
+            middlewares: Vec::new(),
+            router: Router::new(),
+        }
+    }
 }
 
 impl Server {
@@ -28,6 +62,7 @@ impl Server {
             let func = self.router.get_func(req.get_uri(), req.get_method());
 
             let mut resp;
+
             if let Some(func) = func {
                 resp = func(req)
             } else {
