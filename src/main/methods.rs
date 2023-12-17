8ef0dc9eae2961be5854a34fd58ad1a62aba3639
Adrian John <adrianjohn6543@gmail.com>
2023-12-17 21:59:52 +0100

    add every method

--- src/main/methods.rs
+++ src/main/methods.rs
@@ -1,27 +1,44 @@
 use whdp::HttpMethod;
-use wjp::{Deserialize, Serialize};
 
-use crate::helper::{HTTPFunction, RawFunction};
-use crate::ser_servlet::SerializeServlet;
+use crate::helper::HTTPFunction;
 
-pub enum Methods<I: Deserialize, O: Serialize> {
+pub enum Methods {
     Get(HTTPFunction),
-    GetRaw(RawFunction),
-    GetSer(SerializeServlet<I, O>),
     Post(HTTPFunction),
-    PostRaw(RawFunction),
-    PostSer(SerializeServlet<I,O>),
+    Options(HTTPFunction),
+    Connect(HTTPFunction),
+    Delete(HTTPFunction),
+    Put(HTTPFunction),
+    Patch(HTTPFunction),
+    Head(HTTPFunction),
+    Trace(HTTPFunction),
 }
 
-impl<I: Deserialize, O: Serialize> Methods<I, O> {
+impl Methods {
     pub fn get_inner(self) -> HTTPFunction {
         match self {
             Methods::Get(s) => s,
+            Methods::Post(s) => s,
+            Methods::Connect(s) => s,
+            Methods::Delete(s) => s,
+            Methods::Head(s) => s,
+            Methods::Patch(s) => s,
+            Methods::Trace(s) => s,
+            Methods::Options(s) => s,
+            Methods::Put(s) => s
         }
     }
     pub fn get_type(&self) -> HttpMethod {
         match self {
             Methods::Get(_) => HttpMethod::Get,
+            Methods::Post(_) => HttpMethod::Post,
+            Methods::Connect(_) => HttpMethod::Connect,
+            Methods::Delete(_) => HttpMethod::Delete,
+            Methods::Head(_) => HttpMethod::Head,
+            Methods::Patch(_) => HttpMethod::Patch,
+            Methods::Trace(_) => HttpMethod::Trace,
+            Methods::Options(_) => HttpMethod::Options,
+            Methods::Put(_) => HttpMethod::Put
         }
     }
 }
